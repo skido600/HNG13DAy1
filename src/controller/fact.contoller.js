0775dbebd85a1e181c85840f37846c642a845b8e
Chuks <63956312+skido600@users.noreply.github.com>
2025-10-16 21:50:14 +0200

    change

--- src/controller/fact.contoller.js
+++ src/controller/fact.contoller.js
@@ -2,11 +2,11 @@ import { fetchfact } from "../utils/factfunction.js";
 
 const factController = async (_req, res) => {
   const fact = await fetchfact("https://catfact.ninja/fact");
-  console.log(fact);
+
   const user = {
     email: "ebisileonard@gmail.com",
     name: "Ebisi chinecherem leonard",
-    stack: "node js express newbie in goland",
+    stack: "node/express.js  newbie in golang",
   };
   res.status(200).json({
     status: "success",
